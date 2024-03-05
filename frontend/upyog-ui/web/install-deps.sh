276155d34a3b3963c1d766c6ba444033cf7c6430
siyaram-ey <139858234+siyaram-ey@users.noreply.github.com>
2024-03-05 18:04:26 +0530

    added "--ignore-engines" in install-deps.sh

--- frontend/upyog-ui/web/install-deps.sh
+++ frontend/upyog-ui/web/install-deps.sh
@@ -4,7 +4,7 @@ BRANCH="$(git branch --show-current)"
 
 INTERNALS="micro-ui-internals"
 
-cd $INTERNALS && echo "installing packages" && yarn install && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
+cd $INTERNALS && echo "installing packages" && yarn install --ignore-engines && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
 cd ..
 
 rm -rf node_modules
